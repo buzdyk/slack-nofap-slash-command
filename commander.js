6ff8bd7bd1159a5cbd24b214cdd9f0c6fee7c78b
Buzdyk Gregory <buzdyk.gregory@gmail.com>
2019-01-01 15:20:58 +0700

    refactor handler.js

---
+++ commander.js
@@ -0,0 +1,104 @@
+import NFService from './db/nofap-service'
+import ReflectionService from './db/reflection-service'
+import * as db from './db/dynamo'
+import * as msgs from './helpers/responses'
+
+export default class Commander
+{
+    constructor(db) {
+        this.nfService = new NFService(db)
+        this.reflectionService = new ReflectionService(db)
+    }
+
+    async run(cmd, arg, {userid, username, channel}) {
+        this.arg = arg
+        this.userid = userid
+        this.username = username
+        this.channel = channel
+
+        this.currentNF = await this.nfService.getActiveByUserid(userid)
+
+        let cmds = {
+            start: '_start',
+            oopsie: '_oopsie',
+            reflect: '_reflect',
+            top: '_top',
+            stats: '_stats',
+            show: '_show',
+            participants: '_participants',
+            about: '_about'
+        }
+
+        return new Promise((resolve, reject) => {
+            if (cmds[cmd] === undefined) {
+                return resolve(msgs.noFapCmd404())
+            }
+
+            return resolve(this[cmds[cmd]]())
+        })
+    }
+
+    hasCurrentNF() {
+        return this.currentNF !== null && typeof this.currentNF == 'object'
+    }
+
+    async _start() {
+        if (this.hasCurrentNF()) return msgs.startNoFapDuplicate(this.currentNF)
+        let nf = await this.nfService.start(this.userid, this.username)
+        this.arg && await this.reflectionService.reflectOnNF(nf.uuid, this.arg)
+
+        return msgs.noFapStarted(nf, this.arg)
+    }
+
+    async _oopsie() {
+        if (!this.hasCurrentNF()) return msgs.activeNoFap404()
+
+        let nf = await this.nfService.finish(this.currentNF, this.arg)
+        this.arg && await this.reflectionService.reflectOnNF(nf.uuid, this.arg)
+        let reflections = await this.reflectionService.getReflectionsByNFUuid(nf.uuid)
+
+        return msgs.noFapFinished(nf, reflections)
+    }
+
+    async _reflect() {
+        if (!this.hasCurrentNF()) return msgs.activeNoFap404()
+        if (!this.arg) return msgs.genericError('You reflected silently')
+
+        this.reflectionService.reflectOnNF(this.currentNF.uuid, this.arg)
+
+        return msgs.noFapReflection(this.currentNF, this.arg)
+    }
+
+    async _stats() {
+        let stats = await this.nfService.getUserStats(this.userid),
+            nfs = await this.nfService.getByUserid(this.userid)
+
+        return msgs.noFapStats(stats, nfs)
+    }
+
+    async _show() {
+        let nfs = await this.nfService.getByUserid(this.userid),
+            nf = _.find(nfs, {uuid: this.arg}),
+            reflections
+
+        if (!nf) return msgs.genericError('Sorry, requested NoFap is not found!')
+
+        reflections = await this.reflectionService.getReflectionsByNFUuid(nf.uuid)
+
+        return msgs.showNF(nf, reflections)
+    }
+
+    async _top() {
+        let top = await this.nfService.getTop()
+        return msgs.topNF(top)
+    }
+
+    async _participants() {
+        let activeNFs = await this.nfService.getActive()
+        return msgs.participantsList(activeNFs)
+    }
+
+    async _about() {
+        return msgs.noFapAbout()
+    }
+}
--- handler.js
+++ handler.js
@@ -1,94 +1,28 @@
-if (!global._babelPolyfill) {
-    require('babel-polyfill')
-}
-
-import {parsePostBody, post} from './helpers/utils'
-import {
-    noFapStarted, noFapFinished,
-    activeNoFap404, noFapCmd404,
-    startNoFapDuplicate, noFapReflection,
-    genericError, noFapStats, noFapAbout, topNF,
-    showNF, participantsList
-} from './helpers/responses'
-
-import NFService from './db/nofap-service'
-import ReflectionService from './db/reflection-service'
+import {parsePostBody} from './helpers/utils'
 import * as db from './db/dynamo'
+import Commander from './commander'
+import axios from 'axios'
 
-let nfService = new NFService(db)
-let reflectionService = new ReflectionService(db)
+let commander = new Commander(db)
 
 export const nofap = async (event, context, callback) => {
     let data = parsePostBody(event.body),
-        respond = (response) => post(data.response_url, response),
         text = data.text.trim(),
         userid = data.user_id,
-        username = data.user_name,
-        existingNF = await nfService.getActiveByUserid(userid),
-        nf, nfs, reflections
+        username = data.user_name
 
     if (text.indexOf(' ') == -1) text = `${text} `
 
     let command = text.substr(0, text.indexOf(' ')),
-        comment = text.substr(text.indexOf(' ') + 1)
+        argument = text.substr(text.indexOf(' ') + 1)
 
-    switch (command) {
-        case 'start':
-            if (existingNF) { respond(startNoFapDuplicate(existingNF)); break }
-            nf = await nfService.start(userid, username)
-            comment && await reflectionService.reflectOnNF(nf.uuid, comment)
-            respond(noFapStarted(nf, comment))
-            break
-
-        case 'oopsie':
-            if (!existingNF) { respond(activeNoFap404()); break }
-            nf = await nfService.finish(existingNF, comment)
-            comment && await reflectionService.reflectOnNF(nf.uuid, comment)
-            reflections = await reflectionService.getReflectionsByNFUuid(nf.uuid)
-            respond(noFapFinished(nf, reflections))
-            break
-
-        case 'reflect':
-            console.log(existingNF, comment)
-            if (!existingNF) { respond(activeNoFap404()); break }
-            if (!comment)    { respond(genericError('You reflected silently')); break }
-            reflectionService.reflectOnNF(existingNF.uuid, comment)
-            respond(noFapReflection(existingNF, comment))
-            break
-
-        case 'stats':
-            let stats = await nfService.getUserStats(userid)
-            nfs = await nfService.getByUserid(userid)
-            respond(noFapStats(stats, nfs))
-            break
-
-        case 'show':
-            nfs = await nfService.getByUserid(userid)
-            nf = _.find(nfs, {uuid: comment})
-
-            if (!nf) { respond(genericError('Sorry, requested NoFap is not found!')); break }
-
-            reflections = await reflectionService.getReflectionsByNFUuid(nf.uuid)
-            respond(showNF(nf, reflections))
-            break
-
-        case 'top':
-            let top = await nfService.getTop()
-            respond(topNF(top))
-            break
-
-        case 'participants':
-            respond(participantsList(await nfService.getActive()))
-            break
-
-        case 'about':
-            respond(noFapAbout())
-            break
-
-        default:
-            respond(noFapCmd404())
-            break
-    }
+    commander.run(command, argument, {
+        userid, username, channel: data.channel_id
+    }).then(res => {
+        axios.post(data.response_url, res)
+    }).catch(res => {
+        console.log(res)
+    })
 
     // prevent entered slash console command
     // from popping up in the channel
